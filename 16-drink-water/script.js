da28f56fc15203ad35c84d24d8a8c7e35a111ddf
Gordon Hicken <gordon.hicken@gmail.com>
2022-12-06 23:13:41 -0500

    Completed big glass JS with proper grammar

--- 16-drink-water/script.js
+++ 16-drink-water/script.js
@@ -1,7 +1,9 @@
 const smallCups = document.querySelectorAll('.cup-small');
 const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
-const remained = document.getElementById('remained');
+const remaining = document.getElementById('remaining');
+
+updateBigCup();
 
 smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => highlightCups(index));
@@ -21,4 +23,28 @@ function highlightCups(index) {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remaining.style.visibility = 'hidden';
+    remaining.style.height = 0;
+  } else {
+    remaining.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000} Liters`;
+  }
 }
